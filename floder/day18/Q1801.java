c03d966bc4ab9afb003973b3b6afbed1dc4c57ce
민준성 <eak00700@naver.com>
2022-09-30 22:35:56 +0900

    commit

--- floder/day18/Q1801.java
+++ floder/day18/Q1801.java
@@ -15,18 +15,36 @@ class Member{
         this.phone = phone;
         this.password = password;
         this.idx = idx;
-
+    
+    }
 
     }
     
-}
+
 
 public class Q1801 {
     static Scanner sc = new Scanner(System.in);
     static ArrayList<Member> user = new ArrayList<Member>();
     static int idx=0;
+    static void showMenu(){
+        System.out.println("----------------------------");
+        System.out.println("1.전체회원조회");
+        System.out.println("2.회원등록");
+        System.out.println("3.회원검색");
+        System.out.println("4.회원삭제");
+        System.out.println("0.종료");
+        System.out.println("----------------------------");
+        
+    }
     static void showAllUser(){
-       
+        for(int i=0; i<user.size();i++){
+            System.out.println("인덱스 번호 : "+user.get(i).idx);
+            System.out.println("아이디 : "+user.get(i).id);
+            System.out.println("이름 : "+user.get(i).name);
+            System.out.println("전화번호 : "+user.get(i).phone);
+            System.out.println("비밀번호 : "+user.get(i).password);
+
+        }
     }
     static void registerUser(){
         user.add(new Member(sc.next(), sc.next(), sc.next(), sc.next(), ++idx));
@@ -37,7 +55,6 @@ public class Q1801 {
     static void deleteUserByIdx(){
 
     }
-   
     public static void main(String[] args) {
          // ---------------------
     // 1. 전체회원조회
@@ -48,9 +65,11 @@ public class Q1801 {
     // --------------------
     // 실행할 메뉴 입력 >>> 
     
-   
     Member member;
         while(true){
+            showMenu();
+            System.out.print("실행할 메뉴 입력 >>");
+
         
     }
     
