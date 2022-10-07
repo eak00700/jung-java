a8caaf15afd032ad71876d51ac2d8d3ddc9febb4
민준성 <eak00700@naver.com>
2022-10-07 15:12:19 +0900

    commit

--- floder/practice/ch5/Exercise5_11.java
+++ floder/practice/ch5/Exercise5_11.java
@@ -1,5 +1,7 @@
 package practice.ch5;
 
+import java.util.Arrays;
+
 public class Exercise5_11 {
     public static void main(String[] args) {
         int[][] score = {
@@ -12,17 +14,23 @@ public class Exercise5_11 {
     int[][] result = new int[score.length+1][score[0].length];
 
     for(int i=0; i<score.length; i++){
+        int sum = 0; 
         for(int j=0; j<score[i].length; j++){
-                
+                result[i][j] = score[i][j];
+                sum += score[i][j];
+                if(j==score[i].length-1){
+                    result[i][j+1] = sum;
+                }
+
             }
         }
 
-        for(int i=0; i<result.length; i++){
-            for(int j=0; j<result.length; j++){
-                System.out.printf("%4d",result[i][j]);
-            }
-            System.out.println();
-        }
+        // for(int i=0; i<result.length; i++){
+        //     for(int j=0; j<result.length; j++){
+        //         System.out.printf("%4d",result[i][j]);
+        //     }
+            System.out.println(Arrays.toString(result));
+        
     }//main
     
 }
