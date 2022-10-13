9c244e911adcf4af0bca384fe33b3249f401a01a
민준성 <eak00700@naver.com>
2022-10-13 10:28:12 +0900

    javastudy

---
+++ floder/day20/Ex2001.java
@@ -0,0 +1,30 @@
+package day20;
+
+import java.time.Clock;
+
+// AA클래스를 보면서 변수와 메서드를 말할수있어야함.
+class AA{
+    int a; // -->"인스턴스변수"  = "필드 (field)" = 멤버변수
+    static int b; //-->"클래스 변수"
+    void m(){ //-->"인스턴스 메서드"
+
+    }
+    static void m2(){ // -->"클래스 메서드"
+
+    }
+}
+
+public class Ex2001 {
+    public static void main(String[] args) {
+        AA.b = 10;
+        System.out.println(AA.b);
+        AA.m2();
+
+        AA aa = new AA();
+        aa.a = 100;
+        aa.m();
+        System.out.println(aa.b); // 권장 x
+        aa.m2(); //권장 x
+
+    }
+}
---
+++ floder/day20/Ex2002.java
@@ -0,0 +1,31 @@
+package day20;
+
+class Point{
+    int x,y; // 인스턴스변수
+    static int cnt =0; //클래스변수 : 객체가 몇개 생성 되었는지를 카운트.
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+        cnt++;
+    }
+    static void showCnt(){ //클래스 메서드
+        System.out.println("cnt : "+cnt);
+    }
+     void showMe(){
+    System.out.println("점 객체 ("+x+","+y+").");
+    }
+}
+
+
+public  class Ex2002 {
+    public static void main(String[] args) {
+        Point.showCnt();
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(5,4);
+        Point.showCnt();
+        p1.showMe();
+        p2.showMe();
+
+    }
+}
